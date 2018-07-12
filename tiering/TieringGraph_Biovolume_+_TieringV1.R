2f5dcf67d843485e8bd707830be4c91b4ba4f531
SEYIBiodiversity <seyib@es-imac8.stanford.edu>
2018-07-12 15:52:55 -0700

    updated

--- tiering/TieringGraph_Biovolume_+_TieringV1.R
+++ tiering/TieringGraph_Biovolume_+_TieringV1.R
@@ -4,12 +4,13 @@ sizeData$log10_volume <- log10(sizeData$max_vol)
 dim(sizeData)
 head(sizeData)
 timescale <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/timescale.txt')
-plot(1:10,1:10, type="n", xlim=c(550,0), ylim=c(-2,12), xlab="Geological time (Ma)", ylab=expression(paste("Biovolume (log"[10]," cm"^3)))
+par(mar=c(5,4.5,2,2), xaxs="i")
+plot(1:10,1:10, type="n", xlim=c(550,0), ylim=c(-2,12), xlab="Geological time (Ma)", ylab=expression(paste("Biovolume (log"[10]," cm"^3,")")))
 segments(sizeData$fad_age, sizeData$log10_volume, sizeData$lad_age, sizeData$log10_volume)
 myMean <- vector(mode="numeric", length=nrow(timescale))
 my05 <- myMean
 my95 <- myMean
-myCols<- c("red", "orange", "green", "cyan", "magenta", "blue")
+myCols<- c("lightpink", "orange", "lightgreen", "cyan", "magenta", "blue")
 sizeData$color<-myCols[sizeData$tiering]
 head(sizeData)
-segments(sizeData$fad_age, sizeData$log10_volume, sizeData$lad_age, sizeData$log10_volume,col=sizeData$color)
+segments(sizeData$fad_age, sizeData$log10_volume, sizeData$lad_age, sizeData$log10_volume, col=sizeData$color)
