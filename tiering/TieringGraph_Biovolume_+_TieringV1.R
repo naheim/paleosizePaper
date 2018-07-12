3d504baf127876ade120614ae0528589645d3fc2
SEYIBiodiversity <seyib@es-imac8.stanford.edu>
2018-07-12 12:05:10 -0700

    Update

---
+++ tiering/TieringGraph_Biovolume_+_TieringV1.R
@@ -0,0 +1,15 @@
+sizeData <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/bodySizes.txt')
+sizeData <- subset(sizeData, !is.na(tiering) & tiering > 0)
+sizeData$log10_volume <- log10(sizeData$max_vol)
+dim(sizeData)
+head(sizeData)
+timescale <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/timescale.txt')
+plot(1:10,1:10, type="n", xlim=c(550,0), ylim=c(-2,12), xlab="Geological time (Ma)", ylab=expression(paste("Biovolume (log"[10]," cm"^3)))
+segments(sizeData$fad_age, sizeData$log10_volume, sizeData$lad_age, sizeData$log10_volume)
+myMean <- vector(mode="numeric", length=nrow(timescale))
+my05 <- myMean
+my95 <- myMean
+myCols<- c("red", "orange", "green", "cyan", "magenta", "blue")
+sizeData$color<-myCols[sizeData$tiering]
+head(sizeData)
+segments(sizeData$fad_age, sizeData$log10_volume, sizeData$lad_age, sizeData$log10_volume,col=sizeData$color)
