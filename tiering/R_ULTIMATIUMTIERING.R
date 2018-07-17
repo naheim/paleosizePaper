d7b5d5b281f96c5a280a6e1247de5a49cebae92e
Kylo Ren <chris.p.noll@gmail.com>
2018-07-17 08:52:35 -0700

    New Graph!

--- tiering/R_ULTIMATIUMTIERING.R
+++ tiering/R_ULTIMATIUMTIERING.R
@@ -144,20 +144,17 @@ dim(sizeData)
 head(sizeData)
 timescale <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/timescale.txt')
 par(mar=c(5,4.5,2,2), xaxs="i")
-plot(1:10,1:10, type="n", xlim=c(550,0), ylim=c(-2,12), xlab="Geological time (Ma)", ylab=expression(paste("Biovolume (log"[10]," cm"^3,")")))
+plot(1:10,1:10, type="n", xlim=c(550,0), ylim=c(-4,12), xlab="Geological time (Ma)", ylab=expression(paste("Biovolume (log  "[10]," cm"^3,")")))
 segments(sizeData$fad_age, sizeData$log10_volume, sizeData$lad_age, sizeData$log10_volume)
 myMean <- vector(mode="numeric", length=nrow(timescale))
 my05 <- myMean
 my95 <- myMean
-myCols<- c("lightpink", "orange", "lightgreen", "cyan", "magenta", "blue")
+myCols<- c("red", "orange", "green", "cyan", "magenta", "blue")
 sizeData$color<-myCols[sizeData$tiering]
 head(sizeData)
 segments(sizeData$fad_age, sizeData$log10_volume, sizeData$lad_age, sizeData$log10_volume, col=sizeData$color)
-
-
-
-
-
+legend("topleft", lty = 1, title = "Tiering Levels:",legend = c("Tiering Level 1:\n Pelagic", "Tiering Level 2:\n Erect", "Tiering Level 3:\n Surficial", "Tiering Level 4:\n Semi-infaunal", "Tiering Level 5:\n Shallow infaunal", "Tiering Level 6:\n Deep infaunal"),col = c("red", "orange", "green", "cyan", "magenta", "blue"), cex = 0.89)
+mtext(side=3, line=0.5, "Body Size Evolution Catagorized by Tiering Level Over 541 Million-Years", col="black", font=4, cex=1.3)
 
 
 
