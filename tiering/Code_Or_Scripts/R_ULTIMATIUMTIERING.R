a4d02d00990b8013d476d9cb902a7e44185e37af
SEYIBiodiversity <seyib@es-imac8.stanford.edu>
2018-07-23 10:56:27 -0700

    update on tiering codes

--- tiering/Code_Or_Scripts/R_ULTIMATIUMTIERING.R
+++ tiering/Code_Or_Scripts/R_ULTIMATIUMTIERING.R
@@ -7,8 +7,8 @@
 
 #WORKS: 
 
-sizeData <- read.delim("bodySizes.txt") 
-timescale <- read.delim("timescale.txt") 
+sizeData <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/bodySizes.txt') 
+timescale <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/timescale.txt') 
 sizeData = subset(sizeData, !is.na(tiering) & tiering != 0)
 
 n.bins = nrow(timescale)
@@ -33,15 +33,15 @@ legend(540, 1197, legend=c("Tiering Level 1: Pelagic", "Tiering Level 2: Erect",
 
 **************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
 
-#2: Proportion of Each Tiering Lvl Over Time:
+#2: Proportion of Each Tiering Lvl Over Time Stephanie Graph:
 
 #CHANGE DIRECTORIES!
 
 #WORKS:
 
-sizeData <- read.delim("bodySizes.txt") 
+sizeData <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/bodySizes.txt') 
 sizeData <- subset(sizeData, !is.na(tiering) & tiering > 0)
-timescale <- read.delim("timescale.txt") 
+timescale <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/timescale.txt') 
 sizeData$tiering = factor(sizeData$tiering)
 myProp = matrix(NA, nrow = nrow(timescale), ncol = 6)
 my.col = c("red", "orange", "green", "cyan", "magenta", "blue")
@@ -79,8 +79,8 @@ legend(550, 1.16, legend=c("Tiering Level 1: Pelagic", "Tiering Level 2: Erect",
 
 #CHANGE DIRECTORY!
 
-bodySize <- read.delim("bodySizes.txt") 
-timescale <- read.delim("timescale.txt") 
+bodySize <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/bodySizes.txt') 
+timescale <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/timescale.txt') 
 bodySize <- subset(sizeData, !is.na(tiering) & tiering != 0)
 
 library(paleoTS)
@@ -108,7 +108,7 @@ my.n[i,j] <- length(temp.data)
 par(col="black")
 source("https://github.com/naheim/paleosizePaper/raw/master/sharedCode/functions.r")
 time.plot(c(0, 8), expression(paste("Biovolume (log  "[10]," cm"^3,")")), mar = c(3.5, 3.5, 3.5, 3.5)+0.1, mgp = c(2, 0.75, 0))
-#plot(timescale$age_bottom, my.mean[,3], type="n", pch=16, xlab="Geologic Time (Ma)", xlim=c(541, 0), ylab="Mean Size", ylim=c(1.2,6.5), main="Mean expression(paste("Biovolume (log"[10]," cm"^3)"))
+#plot(timescale$age_bottom, my.mean[,3], type="n", pch=16, xlab="Geologic Time (Ma)", xlim=c(541, 0), ylab="Mean Size", ylim=c(1.2,6.5), main="Mean expression(paste("Biovolume (log"[10]," mm"^3)"))
 my.col = c("red", "orange", "green", "cyan", "magenta", "blue")
 for(i in 1:6) {
   my.ts <- as.paleoTS(mm=my.mean[!is.na(my.var[,i]), i], vv=my.var[!is.na(my.var[,i]), i], nn=my.n[!is.na(my.var[,i]), i], tt=my.time[!is.na(my.var[,i])], oldest="last")
@@ -130,7 +130,7 @@ my.col = c("red", "orange", "green", "cyan", "magenta", "blue")
 my.opp = c("chartreuse3", "dodgerblue2", "red", "darkorange1", "forestgreen", "orange2")
 par(mar = c(5.5, 5.5, 2, 2)+0.1)
 par(mgp = c(3, 1.7, 0))
-boxplot(log10(max_vol)~tiering, bodySize, xlab="Tiering Level", ylab=expression(paste("Biovolume (log  "[10]," cm"^3,")")), col=my.col, names=c("Tiering Level 1:\n Pelagic", "Tiering Level 2:\n Erect", "Tiering Level 3:\n Surficial", "Tiering Level 4:\n Semi-infaunal", "Tiering Level 5:\n Shallow infaunal", "Tiering Level 6:\n Deep infaunal"), border = my.opp)
+boxplot(log10(max_vol)~tiering, bodySize, xlab="Tiering Level", ylab=expression(paste("Biovolume (log  "[10]," mm"^3,")")), col=my.col, names=c("Tiering Level 1:\n Pelagic", "Tiering Level 2:\n Erect", "Tiering Level 3:\n Surficial", "Tiering Level 4:\n Semi-infaunal", "Tiering Level 5:\n Shallow infaunal", "Tiering Level 6:\n Deep infaunal"), border = my.opp)
 mtext(side=3, line=0.5, "Biovolume vs. Tiering Level", col="black", font=4, cex=1.3)
 
 **************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
@@ -146,7 +146,7 @@ head(sizeData)
 timescale <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/timescale.txt')
 source("https://github.com/naheim/paleosizePaper/raw/master/sharedCode/functions.r")
 
-time.plot(c(-10, 15), expression(paste("Biovolume (log  "[10]," cm"^3,")")), mar=c(4,4,4,4), mgp = c(2, 0.75, 0))
+time.plot(c(-10, 15), expression(paste("Biovolume (log  "[10]," mm"^3,")")), mar=c(4,4,4,4), mgp = c(2, 0.75, 0))
 
 #plot(1:10,1:10, type="n", xlim=c(550,0), ylim=c(-4,12), xlab="Geological time (Ma)", ylab=expression(paste("Biovolume (log  "[10]," cm"^3,")")))
 
@@ -290,17 +290,43 @@ lines(x=timescale$age_mid,y=meanVector, col="black", lwd=4)
 
 #Stephanie Logistic Regression Combined Tiering Graph 
 
-
-
-
+ source("https://github.com/naheim/paleosizePaper/raw/master/sharedCode/functions.r")
+sizeData <- read.delim(file="https://github.com/naheim/paleosizePaper/raw/master/rawDataFiles/bodySizes.txt")
+timescale <- read.delim(file="https://github.com/naheim/paleosizePaper/raw/master/rawDataFiles/timescale.txt")
+nBins <- nrow(timescale) 
+myCols<- c("red", "orange", "green", "cyan", "magenta", "blue")
+tierLabs <- c("Pelagic", "Erect", "Surficial", "Semi-infaunal", "Shallow infaunal", "Deep infaunal")
+time.plot.mult(nrow=2, ncol=3, plot.width=10, top.mar=2)
+for(j in 1:6){
+	tiering <- subset(sizeData, tiering==j)
+	tierExtSel <- data.frame(matrix(NA, nrow=nBins, ncol=3, dimnames=list(timescale$interval_name, c('coef','ci.minus','ci.plus'))))
+	for(i in 1:nBins) {
+		temp <- subset(tiering, fad_age > timescale$age_top[i] & lad_age < timescale$age_bottom[i])
+		if(nrow(temp) > 0) {
+			temp$extinct <- 0
+			temp$extinct[temp$lad_age >= timescale$age_top[i] & temp$lad_age < timescale$age_bottom[i]] <- 1 
+			if(sum(temp$extinct) >= 3 & nrow(temp) >= 6) {
+				myGlm <- glm(extinct ~ log10(max_vol), data=temp, family=binomial)
+				tierExtSel$coef[i] <- myGlm$coefficients[2]
+				myCi <- confint(myGlm)
+				tierExtSel$ci.minus[i] <- myCi[2,1]
+				tierExtSel$ci.plus[i] <- myCi[2,2]
+			}
+		}
+	}
+	plot(timescale$age_mid[tierExtSel$coef > -5 & tierExtSel$coef < 5], tierExtSel$coef[tierExtSel$coef > -5 & tierExtSel$coef < 5], pch=16, cex=1.25, xaxt="n", xlab="", xlim=c(541,0), col=myCols[j], ylab=" Log-Odds of Extinction")
+	abline(h=0, lty=2)
+	segments(timescale$age_mid,tierExtSel$ci.minus,timescale$age_mid,tierExtSel$ci.plus)
+	title(main= paste("Tiering ", j, ": ", tierLabs[j], sep=""))
+}
 
 
 **************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
 
 #Tiering Akike Walks 
 
-bodySize <- read.delim("bodySizes.txt") 
-timescale <- read.delim("timescale.txt") 
+bodySize <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/bodySizes.txt') 
+timescale <- read.delim(file='https://raw.githubusercontent.com/naheim/paleosizePaper/master/rawDataFiles/timescale.txt') 
 bodySize <- subset(sizeData, !is.na(tiering) & tiering != 0)
 
 library(paleoTS)
