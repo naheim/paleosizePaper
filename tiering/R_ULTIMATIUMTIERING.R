623bdf92e43bee1b60937cde00ff5d3c78baf49c
Kylo Ren <chris.p.noll@gmail.com>
2018-07-23 11:14:16 -0700

    Update!

--- tiering/R_ULTIMATIUMTIERING.R
+++ tiering/R_ULTIMATIUMTIERING.R
@@ -5,10 +5,10 @@
 
 #The Change in the Amount of Gentra Categorized by Tiering Level Over Million-Years
 
-#WORKS: 
-#install.packages("RColorBrewer")
 
-library("RColorBrewer")
+#COLOR YAY: http://projects.susielu.com/viz-palette?colors=[%22#ff5640%22,%22#ffd900%22,%22#00ffd7%22,%22#ee92ed%22,%22#ff00ff%22,%22#0000ff%22]&backgroundColor=%22white%22&fontColor=%22black%22
+#WORKS: 
+
 sizeData <- read.delim("bodySizes.txt") 
 timescale <- read.delim("timescale.txt") 
 sizeData = subset(sizeData, !is.na(tiering) & tiering != 0)
@@ -24,11 +24,11 @@ for(i in 1:nrow(timescale)){temp.data = sizeData$tiering[sizeData$fad_age > time
 	}
 
 par(xaxs = "i", yaxs = "i")
-my.col = c("red", "orange", "green", "cyan", "magenta", "blue")
+my.col = 
 source("https://github.com/naheim/paleosizePaper/raw/master/sharedCode/functions.r")
 time.plot(c(0,range(my.tiering)), "Amount of Genera", mar = c(4, 3.5, 6, 3.5)+0.1, mgp = c(2.5, 0.75, 0))
 #plot(1:10,1:10, type="n", xlim=c(541,-5), ylim=range(my.tiering), pch=21, xlab="Geologic Time (Ma)", ylab="Amount of Gentra")
-for(i in 1:1:6){lines(timescale$age_bottom, my.tiering[,i], col=my.col[i])}
+for(i in 1:1:6){lines(timescale$age_bottom, my.tiering[,i], col=my.col[i], lwd = 4)}
 abline(v = c(65, 200, 251.2, 443.8), col="black")
 mtext(side=3, line=0.5, "The Change in the Amount of Gentra Categorized by Tiering Level Over Million-Years", col="black", font=4, cex=1.3)
 legend(540, 1197, legend=c("Tiering Level 1: Pelagic", "Tiering Level 2: Erect", "Tiering Level 3: Surficial", "Tiering Level 4: Semi-infaunal", "Tiering Level 5: Shallow infaunal", "Tiering Level 6: Deep infaunal"), col = my.col, lty = 1, title="Tiering Levels:", bg = "white", box.col=NA, title.adj = 0.31)
