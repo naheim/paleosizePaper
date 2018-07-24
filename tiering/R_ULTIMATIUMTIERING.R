96aa280b51fd8f8b725ce1172443ca0649091c2a
Kylo Ren <chris.p.noll@gmail.com>
2018-07-24 11:04:20 -0700

    Update!

--- tiering/R_ULTIMATIUMTIERING.R
+++ tiering/R_ULTIMATIUMTIERING.R
@@ -108,19 +108,22 @@ my.n[i,j] <- length(temp.data)
 }
 par(col="black")
 source("https://github.com/naheim/paleosizePaper/raw/master/sharedCode/functions.r")
-time.plot(c(0, 8), expression(paste("Biovolume (log  "[10]," cm"^3,")")), mar = c(3.5, 3.5, 3.5, 3.5)+0.1, mgp = c(2, 0.75, 0))
+time.plot(c(0, 8), expression(paste("Biovolume (log  "[10]," mm"^3,")")), mar = c(4, 3.5, 4, 3.5)+0.5, mgp = c(2.5, 0.75, 0))
+
 #plot(timescale$age_bottom, my.mean[,3], type="n", pch=16, xlab="Geologic Time (Ma)", xlim=c(541, 0), ylab="Mean Size", ylim=c(1.2,6.5), main="Mean expression(paste("Biovolume (log"[10]," cm"^3)"))
-my.col = c("red", "orange", "green", "cyan", "magenta", "blue")
+
+my.col = c("#ff5640","#ffd900","#00ffd7","#ee92ed","#ff00ff","#0000ff")
 for(i in 1:6) {
   my.ts <- as.paleoTS(mm=my.mean[!is.na(my.var[,i]), i], vv=my.var[!is.na(my.var[,i]), i], nn=my.n[!is.na(my.var[,i]), i], tt=my.time[!is.na(my.var[,i])], oldest="last")
   fit3models(my.ts, method="Joint", pool=FALSE)
   #par(col=my.mean$color[k]); par(col="deepskyblue3")
   lines(timescale$age_mid, my.mean[, i], col=my.col[i])
 }
-mtext(side=3, line=0.5, "The Change in Mean Biovolume of Gentra Categorized by Tiering Level Over Million-Years", col="black", font=4, cex=1.3)
-abline(v = c(65, 200, 251.2, 443.8), col="black")
+mtext(side=3, line=0.5, "The Change in Mean Biovolume of Genera Categorized by Tiering Level Over Million-Years", col="black", font=4, cex=1.3)
 par(col="black")
+abline(v = c(65, 200, 251.2, 443.8), col="black")
 legend("topleft", legend=c("Tiering Level 1: Pelagic", "Tiering Level 2: Erect", "Tiering Level 3: Surficial", "Tiering Level 4: Semi-infaunal", "Tiering Level 5: Shallow infaunal", "Tiering Level 6: Deep infaunal"), col = my.col, lty = 1, title="Tiering Level", bg = "white", title.adj = 0.31, cex=1)
+#END
 
 **************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
 
