1d928f33d913cb8e2545006e703732db276826c3
shannonheh <shannonheh26@gmail.com>
2018-07-23 22:01:10 -0700

    edited markdown and compiled code

--- motility/Code/Body Size vs Motility Boxplot.R
+++ motility/Code/Body Size vs Motility Boxplot.R
@@ -7,10 +7,11 @@ sizeDataMod$motile <- 0 #non-motile
 sizeDataMod$motile[(sizeDataMod[,"motility"]==1 | sizeDataMod[,"motility"]==2) & !is.na(sizeDataMod$motility)] <- 1 #motile
 sizeDataMod$motile <- factor(sizeDataMod$motile, levels=c(1,0))
 
-boxplot(log10(sizeDataMod$max_vol)~sizeDataMod$motile,data=sizeDataMod,main = "Body Size vs Motility Boxplot", xlab="Motility Level", ylab="", col=c("blue","red"), names=c("Motile","Nonmotile"))
+boxplot(log10(sizeDataMod$max_vol)~sizeDataMod$motile,data=sizeDataMod,main = "Body Size vs Motility Boxplot", xlab="Motility Level", ylab="", col=c("blue","red"), names=c("Motile","Nonmotile"),notch=TRUE)
 title(ylab=expression(paste("Biovolume log"[10],"mm"^3)), line=2.2)
 
 
-boxplot(log10(sizeDataMod$max_vol)~sizeDataMod$motility,data=sizeDataMod,main = "Body Size vs Motility Level Boxplot", xlab="Motility Level", ylab="", col=c("darkorange", "dodgerblue","yellow", "forestgreen", "red", "purple"))
+boxplot(log10(sizeDataMod$max_vol)~sizeDataMod$motility,data=sizeDataMod,main = "Body Size vs Motility Level Boxplot", xlab="Motility Level", ylab="", col=c("darkorange", "dodgerblue","yellow", "forestgreen", "red", "purple"),notch=TRUE)
 title(ylab=expression(paste("Biovolume log"[10],"mm"^3)), line=2.2)
 
+# The Notch - displays the a confidence interval around the median which is normally based on the median +/- 1.57 x IQR/sqrt of n.  According to Graphical Methods for Data Analysis (Chambers, 1983) although not a formal test the, if two boxes' notches do not overlap there is ‘strong evidence’ (95% confidence) their medians differ.
--- motility/README.md
+++ motility/README.md
@@ -1,2 +1,4 @@
 # Motility
 This directory contains the R code, figures, and text associated with the text on marine animal levels of motility.
+
+Our writing can be found here: https://docs.google.com/document/d/1sXK0NQ-jy6yN-nzLORWxCeURHO4ei8jZRODUBCvq_rA/edit?usp=sharing
